0fa0b57b06698a1591e5b145525fb84a9a394846
keny-stark <abdurakhmanov_i@iuca.kg>
2019-12-14 12:03:58 +0600

    сделал первый этап, модели для картинок и комитов

--- source/main/settings.py
+++ source/main/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken'
     'webapp',
+    'api_v1',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_URL = '/uploads/'
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(null=False, blank=False, upload_to='image_from_user', verbose_name='image')
+    text = models.TextField(null=False, blank=False, verbose_name='about', max_length=2000)
+    user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE, verbose_name='user')
+    like = models.IntegerField(default=0, verbose_name='like')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Time created')
+
+    def __str__(self):
+        return self.text
+
+
+class Commit(models.Model):
+    description = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Description')
+    created_by = models.ForeignKey(User, null=False, blank=False, verbose_name='created by',
+                                   on_delete=models.PROTECT, related_name='created_by')
+    image_for_comment = models.ForeignKey('webapp.Image', related_name='image_for_commit', on_delete=models.SET_NULL,
+                                          verbose_name='image for commit', blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Time of add')
+
+    def __str__(self):
+        return self.description
